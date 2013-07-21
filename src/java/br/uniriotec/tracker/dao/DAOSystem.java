e792d5733c9955b35f73a9904234c8641f6b6499
paulo <paulo@paulo>
2013-07-21 18:46:31 -0300

    Commit do arquivo DAOSystem e das funções createSystem  e changeSystemName As funções não foram testadas mas ainda não estão sendo utilizadas.

---
+++ src/java/br/uniriotec/tracker/dao/DAOSystem.java
@@ -0,0 +1,95 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.uniriotec.tracker.dao;
+
+import static br.uniriotec.tracker.dao.DAOMysqlConector.abrirConexao;
+import static br.uniriotec.tracker.dao.DAOMysqlConector.conn;
+import static br.uniriotec.tracker.dao.DAOMysqlConector.fecharConexao;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author afonso
+ */
+public class DAOSystem extends DAOMysqlConector {
+
+    // CREATE NEW SYSTEM
+    public boolean createSystem(String systemName) {
+
+        abrirConexao();
+
+        String sqlVerifySystem = "";
+        sqlVerifySystem += "SELECT * FROM ticketManager.USER";
+        sqlVerifySystem += " WHERE systemName = " + "'" + systemName + "'";
+
+        String sqlCreateSystem = "";
+        sqlCreateSystem += "INSERT INTO ticketManager.SYSTEM (systemName)";
+        sqlCreateSystem += "VALUES (" + "'"
+                + systemName + "')";
+
+        try {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sqlVerifySystem);
+            if (rs.next()) {
+                System.out.println("SISTEMA JA ESTA CADASTRADO");
+                return false;
+            } else {
+                System.out.println("SISTEMA NAO EXISTE E VAI SER CRIADO");
+                System.out.println(sqlCreateSystem);
+                Statement stUser = conn.createStatement();
+                int rsUser = stUser.executeUpdate(sqlCreateSystem);
+                if (rsUser == 0) {
+                    System.out.println("GRAVOU O NOVO SISTEMA!");
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+
+        fecharConexao();
+        return false;
+    }
+
+    // CHANGE USER NAME 
+    public boolean changeSystemName(String systemName) {
+
+        abrirConexao();
+
+        String sqlVerifySystem = "";
+        sqlVerifySystem += "SELECT * FROM ticketManager.SYSTEM";
+        sqlVerifySystem += " WHERE systemName = " + "'" + systemName + "'";
+
+        String sqlNewSystemName = "";
+        sqlNewSystemName += "INSERT INTO ticketManager.SYSTEM (systemName)";
+        sqlNewSystemName += "VALUES (" + "'"
+                + systemName + "')";
+
+        try {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sqlVerifySystem);
+            if (rs.next()) {
+                System.out.println("JA EXISTE SISTEMA COM O NOME INFORMADO). DIGITE OUTRO NOME.");
+                System.out.println(sqlVerifySystem);
+                return false;
+            } else {
+                Statement stUser = conn.createStatement();
+                int rsUser = stUser.executeUpdate(sqlNewSystemName);
+                return true;
+
+            }
+
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+
+        fecharConexao();
+        return false;
+    }
+}
