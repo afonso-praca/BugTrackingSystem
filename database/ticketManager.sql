a61883b1635416eb45519b19e13bd327045b10fc
afonso-praca <afonsoinfo@gmail.com>
2013-07-21 10:48:09 -0300

    Alter on user table

--- database/ticketManager.sql
+++ database/ticketManager.sql
@@ -1,88 +1,178 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+CREATE DATABASE  IF NOT EXISTS `ticketManager` /*!40100 DEFAULT CHARACTER SET big5 */;
+USE `ticketManager`;
+-- MySQL dump 10.13  Distrib 5.5.31, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: ticketManager
+-- ------------------------------------------------------
+-- Server version	5.5.31-0ubuntu0.12.04.2
 
-CREATE SCHEMA IF NOT EXISTS `ticketManager` DEFAULT CHARACTER SET big5 ;
-USE `ticketManager` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Table `ticketManager`.`COMPONENT`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `ticketManager`.`COMPONENT` (
-  `componentName` VARCHAR(100) NOT NULL ,
-  `operadorEmail` VARCHAR(255) NOT NULL ,
-  PRIMARY KEY (`componentName`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = big5;
+--
+-- Table structure for table `COMPONENT`
+--
 
+DROP TABLE IF EXISTS `COMPONENT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `COMPONENT` (
+  `componentName` varchar(100) NOT NULL,
+  `operadorEmail` varchar(255) NOT NULL,
+  PRIMARY KEY (`componentName`)
+) ENGINE=InnoDB DEFAULT CHARSET=big5;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `ticketManager`.`FUNCTIONALITY`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `ticketManager`.`FUNCTIONALITY` (
-  `idFunctionality` INT(11) NOT NULL ,
-  `functionalityName` VARCHAR(45) NOT NULL ,
-  `description` VARCHAR(100) NOT NULL ,
-  `roleId` INT(11) NOT NULL ,
-  PRIMARY KEY (`idFunctionality`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = big5;
+--
+-- Dumping data for table `COMPONENT`
+--
 
+LOCK TABLES `COMPONENT` WRITE;
+/*!40000 ALTER TABLE `COMPONENT` DISABLE KEYS */;
+/*!40000 ALTER TABLE `COMPONENT` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `ticketManager`.`ROLE`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `ticketManager`.`ROLE` (
-  `type` INT(11) NOT NULL ,
-  `roleTag` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`type`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = big5;
+--
+-- Table structure for table `FUNCTIONALITY`
+--
 
+DROP TABLE IF EXISTS `FUNCTIONALITY`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `FUNCTIONALITY` (
+  `idFunctionality` int(11) NOT NULL,
+  `functionalityName` varchar(45) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `roleId` int(11) NOT NULL,
+  PRIMARY KEY (`idFunctionality`)
+) ENGINE=InnoDB DEFAULT CHARSET=big5;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `ticketManager`.`SYSTEM`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `ticketManager`.`SYSTEM` (
-  `systemName` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`systemName`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = big5;
+--
+-- Dumping data for table `FUNCTIONALITY`
+--
 
+LOCK TABLES `FUNCTIONALITY` WRITE;
+/*!40000 ALTER TABLE `FUNCTIONALITY` DISABLE KEYS */;
+/*!40000 ALTER TABLE `FUNCTIONALITY` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `ticketManager`.`TICKET`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `ticketManager`.`TICKET` (
-  `idTicket` INT(11) NOT NULL ,
-  `title` VARCHAR(255) NOT NULL ,
-  `systemKey` VARCHAR(45) NOT NULL ,
-  `componentKey` VARCHAR(45) NOT NULL ,
-  `description` VARCHAR(45) NOT NULL ,
-  `status` INT(11) NOT NULL ,
-  `operador` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`idTicket`) ,
-  UNIQUE INDEX `idTicket_UNIQUE` (`idTicket` ASC) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = big5;
+--
+-- Table structure for table `ROLE`
+--
 
+DROP TABLE IF EXISTS `ROLE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ROLE` (
+  `type` int(11) NOT NULL,
+  `roleTag` varchar(45) NOT NULL,
+  PRIMARY KEY (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=big5;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `ticketManager`.`USER`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `ticketManager`.`USER` (
-  `idUser` INT(10) UNSIGNED NOT NULL ,
-  `name` VARCHAR(45) NOT NULL ,
-  `lastName` VARCHAR(45) NOT NULL ,
-  `email` VARCHAR(255) NOT NULL ,
-  `password` VARCHAR(12) NOT NULL ,
-  PRIMARY KEY (`idUser`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = big5
-COMMENT = 'Tabela de usuarios do sistema';
+--
+-- Dumping data for table `ROLE`
+--
 
+LOCK TABLES `ROLE` WRITE;
+/*!40000 ALTER TABLE `ROLE` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ROLE` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `SYSTEM`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+DROP TABLE IF EXISTS `SYSTEM`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `SYSTEM` (
+  `systemName` varchar(45) NOT NULL,
+  PRIMARY KEY (`systemName`)
+) ENGINE=InnoDB DEFAULT CHARSET=big5;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `SYSTEM`
+--
+
+LOCK TABLES `SYSTEM` WRITE;
+/*!40000 ALTER TABLE `SYSTEM` DISABLE KEYS */;
+/*!40000 ALTER TABLE `SYSTEM` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `TICKET`
+--
+
+DROP TABLE IF EXISTS `TICKET`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `TICKET` (
+  `idTicket` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `systemKey` varchar(45) NOT NULL,
+  `componentKey` varchar(45) NOT NULL,
+  `description` varchar(45) NOT NULL,
+  `status` int(11) NOT NULL,
+  `operador` varchar(45) NOT NULL,
+  PRIMARY KEY (`idTicket`),
+  UNIQUE KEY `idTicket_UNIQUE` (`idTicket`)
+) ENGINE=InnoDB DEFAULT CHARSET=big5;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `TICKET`
+--
+
+LOCK TABLES `TICKET` WRITE;
+/*!40000 ALTER TABLE `TICKET` DISABLE KEYS */;
+/*!40000 ALTER TABLE `TICKET` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `USER`
+--
+
+DROP TABLE IF EXISTS `USER`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `USER` (
+  `name` varchar(45) NOT NULL,
+  `lastName` varchar(45) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(12) NOT NULL,
+  `token` varchar(8) DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=big5 COMMENT='Tabela de usuarios do sistema';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `USER`
+--
+
+LOCK TABLES `USER` WRITE;
+/*!40000 ALTER TABLE `USER` DISABLE KEYS */;
+INSERT INTO `USER` VALUES ('afonso','praca','afonsoinfo@gmail.com','123456','13076639'),('Daniela','Soria','dani@soria.com','123','8bbd3219');
+/*!40000 ALTER TABLE `USER` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-07-21 10:46:26
