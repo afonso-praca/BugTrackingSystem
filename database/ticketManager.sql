911e623e531c2f8c04be6a9c66f3511ac5099d66
paulo <paulo@paulo>
2013-07-21 16:02:18 -0300

    Adição do campos isActive,forcePassReset,shouldChangePassword.

--- database/ticketManager.sql
+++ database/ticketManager.sql
@@ -2,7 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `ticketManager` /*!40100 DEFAULT CHARACTER SET bi
 USE `ticketManager`;
 -- MySQL dump 10.13  Distrib 5.5.31, for debian-linux-gnu (x86_64)
 --
--- Host: 127.0.0.1    Database: ticketManager
+-- Host: localhost    Database: ticketManager
 -- ------------------------------------------------------
 -- Server version	5.5.31-0ubuntu0.12.04.2
 
@@ -16,29 +16,6 @@ USE `ticketManager`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `COMPONENT`
---
-
-DROP TABLE IF EXISTS `COMPONENT`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `COMPONENT` (
-  `componentName` varchar(100) NOT NULL,
-  `operadorEmail` varchar(255) NOT NULL,
-  PRIMARY KEY (`componentName`)
-) ENGINE=InnoDB DEFAULT CHARSET=big5;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `COMPONENT`
---
-
-LOCK TABLES `COMPONENT` WRITE;
-/*!40000 ALTER TABLE `COMPONENT` DISABLE KEYS */;
-/*!40000 ALTER TABLE `COMPONENT` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `FUNCTIONALITY`
@@ -66,6 +43,37 @@ LOCK TABLES `FUNCTIONALITY` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `USER`
+--
+
+DROP TABLE IF EXISTS `USER`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `USER` (
+  `name` varchar(45) NOT NULL,
+  `lastName` varchar(45) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(12) NOT NULL,
+  `token` varchar(8) DEFAULT NULL,
+  `type` varchar(3) NOT NULL,
+  `isActive` binary(1) NOT NULL,
+  `forcePassReset` binary(1) NOT NULL,
+  `shouldChangePassword` binary(1) NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=big5 COMMENT='Tabela de usuarios do sistema';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `USER`
+--
+
+LOCK TABLES `USER` WRITE;
+/*!40000 ALTER TABLE `USER` DISABLE KEYS */;
+INSERT INTO `USER` VALUES ('Paulo','Cesar','paulo.inacio@uniriotec.br','123',NULL,'USR','0','1','1');
+/*!40000 ALTER TABLE `USER` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `ROLE`
 --
 
@@ -86,28 +94,6 @@ CREATE TABLE `ROLE` (
 LOCK TABLES `ROLE` WRITE;
 /*!40000 ALTER TABLE `ROLE` DISABLE KEYS */;
 /*!40000 ALTER TABLE `ROLE` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `SYSTEM`
---
-
-DROP TABLE IF EXISTS `SYSTEM`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `SYSTEM` (
-  `systemName` varchar(45) NOT NULL,
-  PRIMARY KEY (`systemName`)
-) ENGINE=InnoDB DEFAULT CHARSET=big5;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `SYSTEM`
---
-
-LOCK TABLES `SYSTEM` WRITE;
-/*!40000 ALTER TABLE `SYSTEM` DISABLE KEYS */;
-/*!40000 ALTER TABLE `SYSTEM` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -140,34 +126,48 @@ LOCK TABLES `TICKET` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `USER`
+-- Table structure for table `COMPONENT`
 --
 
-DROP TABLE IF EXISTS `USER`;
+DROP TABLE IF EXISTS `COMPONENT`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `USER` (
-  `idUser` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `lastName` varchar(45) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(12) NOT NULL,
-  `type` varchar(45) NOT NULL,
-  `isActive` binary(1) NOT NULL,
-  `forcePassReset` binary(1) NOT NULL,
-  `shouldChangePassword` binary(1) NOT NULL,
-  PRIMARY KEY (`idUser`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=big5 COMMENT='Tabela de usuarios do sistema';
+CREATE TABLE `COMPONENT` (
+  `componentName` varchar(100) NOT NULL,
+  `operadorEmail` varchar(255) NOT NULL,
+  PRIMARY KEY (`componentName`)
+) ENGINE=InnoDB DEFAULT CHARSET=big5;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `USER`
+-- Dumping data for table `COMPONENT`
 --
 
-LOCK TABLES `USER` WRITE;
-/*!40000 ALTER TABLE `USER` DISABLE KEYS */;
-INSERT INTO `USER` VALUES ('afonso','praca','afonsoinfo@gmail.com','123456','13076639'),('Daniela','Soria','dani@soria.com','123','8bbd3219');
-/*!40000 ALTER TABLE `USER` ENABLE KEYS */;
+LOCK TABLES `COMPONENT` WRITE;
+/*!40000 ALTER TABLE `COMPONENT` DISABLE KEYS */;
+/*!40000 ALTER TABLE `COMPONENT` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `SYSTEM`
+--
+
+DROP TABLE IF EXISTS `SYSTEM`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `SYSTEM` (
+  `systemName` varchar(45) NOT NULL,
+  PRIMARY KEY (`systemName`)
+) ENGINE=InnoDB DEFAULT CHARSET=big5;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `SYSTEM`
+--
+
+LOCK TABLES `SYSTEM` WRITE;
+/*!40000 ALTER TABLE `SYSTEM` DISABLE KEYS */;
+/*!40000 ALTER TABLE `SYSTEM` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -179,4 +179,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-07-21 10:46:26
+-- Dump completed on 2013-07-21 15:38:27
