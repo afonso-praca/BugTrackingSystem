4e3b251aec1f89843d7213e32a45c834d77d5533
helanio <helanio@helanio.(none)>
2013-08-07 05:46:28 -0300

    esboço de lista de ticket apos login

--- src/java/br/uniriotec/tracker/dao/DAOTicket.java
+++ src/java/br/uniriotec/tracker/dao/DAOTicket.java
@@ -9,6 +9,8 @@ import static br.uniriotec.tracker.dao.DAOMysqlConector.conn;
 import br.uniriotec.tracker.model.Ticket;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -100,5 +102,34 @@ public class DAOTicket extends DAOMysqlConector{
          fecharConexao();
          return false;
     }
+    
+    public List getAllTickets(){
+        abrirConexao();
+        
+        String sqlQuery = "SELECT * FROM TICKET";
+        List tickets = new ArrayList<Ticket>();
+        
+        try {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sqlQuery);
+            
+            while(rs.next()){
+                int id = rs.getInt("idTicket");
+                String title = rs.getString("title");
+                String system = rs.getString("systemKey");
+                String component = rs.getString("componentKey");
+                String description = rs.getString("description");
+                String status = rs.getString("status");
+                String operator = rs.getString("operador");
+                tickets.add(new Ticket(id, title, system, component, description, status, operator));
+            }
+        } catch (Exception e) {
+                System.err.println(e);
+        }
+        fecharConexao();
+        return tickets;
+    }
+    
+
     
 }
